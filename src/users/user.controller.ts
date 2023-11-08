737fced8fba71fc4cba260f97dc3728f4ef3b202
Anderson Assunção <andersnn7@gmail.com>
2023-11-08 03:15:49 -0500

    update 01

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,9 +1,39 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, Query } from "@nestjs/common";
+import { randomUUID } from "crypto";
 
-@Controller()
+type params = {
+  id: string;
+  idEmpresa: string;
+}
+
+type queryUser = {
+  p: string
+  r: string
+}
+
+type bodyUser = {
+  name: string
+  age: number
+}
+
+@Controller('/users')
 export class UserController {
-  @Get("/teste")
-  helloUser() {
-    return "Teste";
+  @Get("/:id/:idEmpresa")
+  findById(@Param() params: params) {
+    return 'This action returns a user' + 'Id USER: ' + params.id + 'ID Empresa: ' + params.idEmpresa;
+  }
+
+  @Get('/findByPages')
+  findByPages(@Query() queries: queryUser) {
+    return 'Queries' + JSON.stringify(queries);
+  }
+
+  @Post('/create')
+  create(@Body() data: bodyUser) {
+    return {
+      ...data,
+      id: randomUUID(),
+      msg: 'User created successfully'
+    }
   }
 }
