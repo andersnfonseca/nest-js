15fefa9b9b26c94b38ca0146f6b9f8150dc5c837
Anderson Assunção <andersnn7@gmail.com>
2023-11-10 04:39:08 -0500

    pipe

---
+++ src/users/pipe/create-user.validation.pipe.ts
@@ -0,0 +1,14 @@
+import { ArgumentMetadata, HttpException, HttpStatus, Injectable, PipeTransform } from "@nestjs/common";
+import { CreateUserDTO } from "../dto/user.dto";
+
+@Injectable()
+export class CreateUserValidationPipe implements PipeTransform {
+    transform({name, email, username, password}: CreateUserDTO, metadata: ArgumentMetadata) {
+        
+        if (!name || !email || !username || !password) {
+            throw new HttpException('Missing parameter', HttpStatus.UNPROCESSABLE_ENTITY)
+        }
+
+        return {name, email, username, password}
+    }
+}
--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,12 +1,14 @@
-import { Body, Controller, Post } from "@nestjs/common/decorators";
+import { Body, Controller, Post, UsePipes } from "@nestjs/common/decorators";
 import { CreateUserUseCase } from "./useCases/create-user.usecase";
 import { CreateUserDTO } from "./dto/user.dto";
+import { CreateUserValidationPipe } from "./pipe/create-user.validation.pipe";
 
 @Controller('/users')
 export class UserController {
   constructor(private createUserUseCase: CreateUserUseCase) {}
 
   @Post()
+  @UsePipes(new CreateUserValidationPipe())
   async create(@Body() data: CreateUserDTO) {
     return await this.createUserUseCase.execute(data)
   } 
