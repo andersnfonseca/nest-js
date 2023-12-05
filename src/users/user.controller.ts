b7a986f34e51c50fd85ea3fe2803aa7a61bbfeef
Anderson Assunção <andersnn7@gmail.com>
2023-12-05 08:41:20 -0500

    add multer

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,11 +1,12 @@
-import { Body, Controller, Get, Post, UseGuards, UsePipes } from "@nestjs/common/decorators";
+import { Body, Controller, Get, Post, UploadedFile, UseGuards, UseInterceptors, UsePipes } from "@nestjs/common/decorators";
 import { CreateUserUseCase } from "./useCases/create-user.usecase";
-import { CreateUserDTO } from "./dto/user.dto";
+import { CreateUserDTO, FileDTO } from "./dto/user.dto";
 import { CreateUserValidationPipe } from "./pipe/create-user.validation.pipe";
 import { AuthGuard } from "src/providers/auth-guard.provider";
 import { Request } from "@nestjs/common/decorators";
 import { ProfileUserUseCase } from "./useCases/profile-user.usecase";
 import { CreateUserResponseSchemaDTO, CreateUserSchemaDTO } from "./schemas/create-user.schema";
+import { FileInterceptor } from "@nestjs/platform-express";
 
 @Controller("/users")
 export class UserController {
@@ -20,8 +21,16 @@ export class UserController {
  
   @Get("/profile")
   @UseGuards(AuthGuard)
+  
   async profile(@Request() req) {
      return await this.profileUserUseCase.execute(req.user.id)
+  }
+
+  @Post("/avatar")
+  @UseGuards(AuthGuard)
+  @UseInterceptors(FileInterceptor("file"))
+  async uploadAvatar(@Request() req, @UploadedFile() file: FileDTO) {
+      console.log(file)
   }
 }
 
